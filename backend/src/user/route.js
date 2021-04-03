fc4d06413a3c17ccfbb827e3aab694602f31c742
amoutai <amin.moutai@gmail.com>
2021-04-03 21:47:31 +0200

    adding the delete

--- backend/src/user/route.js
+++ backend/src/user/route.js
@@ -18,17 +18,17 @@ router.post('/login', async (req, res) => {
 
     await User.findOne({ username, password })
         .then(user => {
-			if(user) {
-            const id = user._id
-            const token = jwt.sign({ id }, jwtKey, {
-                algorithm: 'HS256',
-                expiresIn: jwtExpires
-            })
-            res.cookie('token', token, { maxAge: jwtExpires * 1000 })
-            res.status(200).send()
-			} else {
-					throw new Error('No user found')
-			}
+            if (user) {
+                const id = user._id
+                const token = jwt.sign({ id }, jwtKey, {
+                    algorithm: 'HS256',
+                    expiresIn: jwtExpires
+                })
+                res.cookie('token', token, { maxAge: jwtExpires * 1000 })
+                res.status(200).send()
+            } else {
+                throw new Error('No user found')
+            }
         })
         .catch(e => {
             console.error(e)
@@ -39,11 +39,11 @@ router.post('/login', async (req, res) => {
         })
 })
 
-router.get('/logout', async (req, res) => {	
-	if(req.cookies.token) {
-		delete req.cookies.token
-	}
-	res.status(200).send()
+router.get('/logout', async (req, res) => {
+    if (req.cookies.token) {
+        delete req.cookies.token
+    }
+    res.status(200).send()
 })
 
 router.get('/user', async (req, res) => {
@@ -55,29 +55,29 @@ router.get('/user', async (req, res) => {
         })
     }
 
-	jwt.verify(token, jwtKey, async (err, data) => {
-		if(err) {
-			res.status(404).send({
-				message: err
-				})
-		}
-		
-		await User.findById(data.id)
-	        .then(user => {
-   		         if (user) {
-   		             res.status(200).send({
-       		             message: 'This is the user you have asked for',
-           		         user
-               		})
-            	} else {
-                	throw new Error('There is no user with this id, please make sure to enter the right id')
-            	}
-        	})
-        	.catch(e => res.status(404).send({
-           		 message: 'Error while trying to get the user',
-            	user: e
-        	}))
-		})
+    jwt.verify(token, jwtKey, async (err, data) => {
+        if (err) {
+            res.status(404).send({
+                message: err
+            })
+        }
+
+        await User.findById(data.id)
+            .then(user => {
+                if (user) {
+                    res.status(200).send({
+                        message: 'This is the user you have asked for',
+                        user
+                    })
+                } else {
+                    throw new Error('There is no user with this id, please make sure to enter the right id')
+                }
+            })
+            .catch(e => res.status(404).send({
+                message: 'Error while trying to get the user',
+                user: e
+            }))
+    })
 })
 
 router.post('/user', async (req, res) => {
@@ -88,11 +88,11 @@ router.post('/user', async (req, res) => {
         if (newUser) {
             await newUser.save()
                 .then(user => {
-						res.status(200).send({
-                    	message: 'The user has successfully created',
-                    	user
-                	})
-				})
+                    res.status(200).send({
+                        message: 'The user has successfully created',
+                        user
+                    })
+                })
                 .catch(e => res.status(400).send({
                     message: 'An error occurred while trying to save the user in the database',
                     error: e
@@ -109,63 +109,78 @@ router.post('/user', async (req, res) => {
 })
 
 router.put('/user', async (req, res) => {
-	if(!req.cookies.token) {
-		res.status(404).send({
-			message: 'No token found'
-		})
-	}
-
-	const {token}= req.cookies
-	jwt.verify(token, jwtKey, async (err, data) => {
-		if(err) {
-			res.status(404).send('An error occurred')
-		}
-
-		const id = data.id
-
-    const fieldsToUpdate = Object.entries(req.body)
-
-    await User.findById(id)
-        .then(response => {
-            if (response) {
-                fieldsToUpdate.forEach(field => response[field[0]] = field[1])
-                response.save()
-                res.send({
-                    message: 'The user have been updated successfully',
-                    user: response
-                })
-            } else {
-                throw new Error('The user can\'t be found')
-            }
+    if (!req.cookies.token) {
+        res.status(404).send({
+            message: 'No token found'
         })
-        .catch(e => {
-            console.log(e)
-            res.status(404).send({
-                message: 'Please, provided valid information',
-                e
+    }
+
+    const { token } = req.cookies
+    jwt.verify(token, jwtKey, async (err, data) => {
+        if (err) {
+            res.status(404).send('An error occurred')
+        }
+
+        const id = data.id
+
+        const fieldsToUpdate = Object.entries(req.body)
+
+        await User.findById(id)
+            .then(response => {
+                if (response) {
+                    fieldsToUpdate.forEach(field => response[field[0]] = field[1])
+                    response.save()
+                    res.send({
+                        message: 'The user have been updated successfully',
+                        user: response
+                    })
+                } else {
+                    throw new Error('The user can\'t be found')
+                }
             })
-        })
-	})
+            .catch(e => {
+                console.log(e)
+                res.status(404).send({
+                    message: 'Please, provided valid information',
+                    e
+                })
+            })
+    })
 })
 
-router.delete('/user/:id', async (req, res) => {
-    const { id } = req.params
-
-    await User.findByIdAndDelete(id)
-        .then(user => {
-            if (user) {
-                res.status(200).send({
-                    message: 'The user has been successfully deleted',
-                    user
-                })
-            } else {
-                throw new Error('The user could not be found')
-            }
+router.delete('/user', async (req, res) => {
+    if (!req.cookies.token) {
+        res.status(404).send({
+            message: 'Unable to find the user'
         })
-        .catch(e => res.status(404).send({
-            message: 'An error has occurred while deleting the user',
-            user: e
-        }))
+    }
+
+    const { token } = req.cookies
+
+    jwt.verify(token, jwtKey, async (err, data) => {
+        if (err) {
+            res.status(500).send({
+                message: 'An error occurred',
+                error: err
+            })
+        }
+        await User.findByIdAndDelete(data.id)
+            .then(user => {
+                if (user) {
+                    res.status(200).send({
+                        message: 'The user has been successfully deleted',
+                        user
+                    })
+                } else {
+                    throw new Error('The user could not be found')
+                }
+            })
+            .catch(e => res.status(404).send({
+                message: 'An error has occurred while deleting the user',
+                user: e
+            }))
+
+    })
 })
 
 module.exports = router
